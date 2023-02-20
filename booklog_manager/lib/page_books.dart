3e0a75c09240bf9c28c46ae06110aca6ad640a75
kimi-soft-dev <kimi@kimi-soft.com>
2023-02-20 19:57:24 +0900

    下部にタブを追加

---
+++ booklog_manager/lib/page_books.dart
@@ -0,0 +1,74 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class PageBooks extends StatefulWidget {
+
+  @override
+  State<PageBooks> createState() => _PageBooksState();
+}
+
+class _PageBooksState extends State<PageBooks> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: requestAPI,
+        tooltip: 'タイトル検索',
+        child: const Icon(Icons.search),
+      ),
+    );
+  }
+
+  void showProgressDialog() {
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: false,
+      transitionDuration: Duration(milliseconds: 300),
+      barrierColor: Colors.black.withOpacity(0.5),
+      pageBuilder: (BuildContext context, Animation animation, Animation secondaryAnimation) {
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+    );
+  }
+
+  Future<void> requestAPI() async {
+    showProgressDialog();
+
+    try {
+      var url = Uri.parse('http://api.booklog.jp/v2/json/2e2afb17c0ba25d2');
+      var response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        print('response: ${response.body}');
+
+        Map<String, dynamic> responseJson = convert.jsonDecode(response.body);
+        List<dynamic> books = List.from(responseJson['books']);
+        books.forEach((jsonObj) {
+            Book book = Book.fromJson(jsonObj);
+        });
+      } else {
+        print('error');
+      }
+    } finally {
+      Navigator.of(context).pop();
+    }
+  }
+}
+
+class Book {
+  final String url;
+  final String title;
+  final String image;
+  final String catalog;
+
+  Book(this.url, this.title, this.image, this.catalog);
+
+  Book.fromJson(Map<String, dynamic> json)
+    : url = json['url'],
+      title = json['title'],
+      image = json['image'],
+      catalog = json['catalog'];
+}
---
+++ booklog_manager/lib/page_settings.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PageSettings extends StatefulWidget {
+  const PageSettings({Key? key}) : super(key: key);
+
+  @override
+  State<PageSettings> createState() => _PageSettingsState();
+}
+
+class _PageSettingsState extends State<PageSettings> {
+  bool lockAppSwitchVal = true;
+
+  void _openUrl() async {
+    const url = 'https://booklog.jp';
+
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: true,
+        forceWebView: false,
+      );
+    } else {
+      throw 'このURLにはアクセスできません';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('ユーザID'),
+                  subtitle: Text('2e2afb17c0ba25d2'),
+                  onTap: () {
+                    print('tap');
+                  },
+                ),
+                const Divider(),
+                ListTile(
+                  leading: const Icon(Icons.image),
+                  title: const Text('カバー画像の表示'),
+                  subtitle: Text(''),
+                  onTap: () {
+                    setState(() {
+                        lockAppSwitchVal = !lockAppSwitchVal;
+                    });
+                  },
+                  trailing: Switch(
+                    value: lockAppSwitchVal,
+                    onChanged: (val) {
+                      setState(() {
+                          lockAppSwitchVal = val;
+                      });
+                    }
+                  ),
+                ),
+                const Divider(),
+                ListTile(
+                  leading: Icon(Icons.language),
+                  title: Text('ブクログへ'),
+                  subtitle: Text('ブラウザが起動します'),
+                  onTap: () => _openUrl(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
